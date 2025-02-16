59d70ceb037c05f11548443acf9b599821d390a6
PedroLudovic144 <pedroludovic5@gmail.com>
2025-02-16 20:44:37 -0300

    New commit

--- PAM/Pedro/Models/Enums/ClasseEnum.cs
+++ PAM/Pedro/Models/Enums/ClasseEnum.cs
@@ -2,6 +2,8 @@ namespace Models.Enums
 {
     public enum ClasseEnum
     {
-        
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3,
     }
 }
--- PAM/Pedro/Models/Personagem.cs
+++ PAM/Pedro/Models/Personagem.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RpgApi.Models.Enums;
 
 namespace Models
 {
     public class Personagem
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
     }
 }
